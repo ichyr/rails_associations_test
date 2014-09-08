775008a8b2951a09b588d71383c233adf9566595
ichyr <ichyr@softserveinc.com>
2014-09-08 07:46:03 +0300

    tested positive vmilist creation

--- features/specialization.feature
+++ features/specialization.feature
@@ -2,9 +2,16 @@ Feature: Testing of vmilosti
 
 	Scenario: Test of index page 
 	 	Given I have vmilosti called "Кінна", "Військова"
-    When I go to the list of specializations
+    When I am on vmilosti list page
     Then I should see "Кінна", "Військова"
 
 	Scenario: Test of creation of vmilist
+		Given I am on vmilosti list page
+		And I click on link "New Vmilist"
+    When I fill in vmilist name with "Таркаси"
+    And I click on button "Create Vmilist"
+    And I am on vmilosti list page
+    Then I should see "Таркаси"
+
 	Scenario: Test of vmilist deletion
 	Scenario: Test of vmilist edition
--- features/step_definitions/specialization_steps.rb
+++ features/step_definitions/specialization_steps.rb
@@ -1,16 +1,28 @@
 Given(/^I have vmilosti called (.*?)$/) do |list|
-  list = list.split(", ")
-  list.each { |entity| 
+  list.tr('"','').split(", ").each { |entity| 
   	Vmilist.create name: entity
   }
 end
 
-When(/^I go to the list of specializations$/) do
+
+Then(/^I should see (.*?)$/) do |data|
+  data.tr('"','').split(", ").each { |text|
+  	expect(page).to have_text text
+  }
+end
+
+Given(/^I am on vmilosti list page$/) do
   visit "/vmilists"
 end
 
-Then(/^I should see (.*?)$/) do |data|
-  data.split(", ").each { |text|
-  	expect(page).to have_text text
-  }
+Given(/^I click on link "(.*?)"$/) do |arg1|
+  click_link arg1
+end
+
+When(/^I click on button "(.*?)"$/) do |arg1|
+  click_button arg1
+end
+
+When(/^I fill in vmilist name with (.*?)$/) do |arg1|
+  fill_in "vmilist[name]", :with => arg1
 end
